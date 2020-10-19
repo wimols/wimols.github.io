663ebf299fa5e9d0c539cfa802190a23f732cf26
wimols <wimwatcherwilly@gmail.com>
2020-10-19 22:04:27 +0200

    website update

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
 
 :choice
-set /P c=Are you sure you want to continue[Y/N]?
+set /P c=Are you sure you want to deploy your changes[Y/N]?
 if /I "%c%" EQU "Y" goto :somewhere
 if /I "%c%" EQU "N" goto :somewhere_else
 goto :choice
 
 
 :somewhere
+echo "Deploying changes"
 git add *
 git commit -m "website update"
 git push
